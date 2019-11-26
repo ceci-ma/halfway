06fc1154f27885b5d5bf8afec4f19021452e7b2e
Molly Daguilar <mdaguilar66@gmail.com>
2019-11-26 15:19:37 +0000

    adding favourites to favourite page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,5 +1,9 @@
 class FavouritesController < ApplicationController
   def index
-    @favourites = Favourite.all
+    # @all_favourites = Favourite.all
+    # @favourites = @all_favourites.select do |favourite|
+    #   favourite.user == current_user
+    # end
+    @favourites = Favourite.where(user: current_user)
   end
 end
