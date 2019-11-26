6860cd75fb5783509275d68aa34a3f3baaf0b811
badenis19 <52892878+badenis19@users.noreply.github.com>
2019-11-26 15:33:44 +0000

    Update favourites_controller.rb

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,9 +1,5 @@
 class FavouritesController < ApplicationController
   def index
-    # @all_favourites = Favourite.all
-    # @favourites = @all_favourites.select do |favourite|
-    #   favourite.user == current_user
-    # end
     @favourites = Favourite.where(user: current_user)
   end
 end
