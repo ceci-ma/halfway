b7ca05df62a1489fb34fff1b81492d4a7d005d90
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-11-27 11:52:23 +0000

    share and get there button working

--- app/javascript/packs/clipboard_to_share.js
+++ app/javascript/packs/clipboard_to_share.js
@@ -1,20 +1,18 @@
 const clipboard_to_share = () => {
   // const button = document.getElementById('share')
-  const sharebtn = document.getElementById('share')
+  const sharebtn = document.getElementById('share');
 
   sharebtn.addEventListener('click', (e) => {
-    var copyText = document.getElementById("copyinput");
-
-      /* Select the text field */
-      copyText.select();
-      copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-      /* Copy the text inside the text field */
-      document.execCommand("copy");
-
-      /* Alert the copied text */
-      alert("Copied the text: " + copyText.value);
-      console.log(copyText.value);
+      const el = document.createElement('textarea');
+      el.value = window.location.href;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      alert("Copied the text: " + el.value);
   })
 }
 
