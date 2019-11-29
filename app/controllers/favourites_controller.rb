de6fc23b93e6f6c6867d45b191afc7ddbae874d4
Lucas Chanet <lucaschanet@gmail.com>
2019-11-29 16:09:47 +0000

    card

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -19,7 +19,6 @@ class FavouritesController < ApplicationController
     @favourite.user = current_user
     @favourite.save
     redirect_to venue_path(@venue)
-    redirect_to current_path
   end
 
   def destroy
