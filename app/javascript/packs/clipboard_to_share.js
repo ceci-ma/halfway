98444dc6d97b465e2b39710a24bf5b009fa34f00
Cecilia Marcellesi <ceciliamarcellesi@gmail.com>
2019-12-02 17:08:54 +0000

    Fix share button event listener

--- app/javascript/packs/clipboard_to_share.js
+++ app/javascript/packs/clipboard_to_share.js
@@ -1,19 +1,22 @@
 const clipboard_to_share = () => {
- const button = document.getElementById('share')
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    if (navigator.share) {
-      navigator.share({
-        title: document.title,
-        text: "Meet me halfway!",
-        url: window.location.href,
-      })
-        .then(() => console.log('Successful share'))
-        .catch((error) => console.log('Error sharing', error));
-    } else {
-    }
-  });
+
+ const button = document.getElementById('share')
+ if (button){
+   button.addEventListener('click', (e) => {
+     e.preventDefault();
+
+     if (navigator.share) {
+       navigator.share({
+         title: document.title,
+         text: "Meet me halfway!",
+         url: window.location.href,
+       })
+         .then(() => console.log('Successful share'))
+         .catch((error) => console.log('Error sharing', error));
+     } else {
+     }
+   });}
 }
 
 export { clipboard_to_share };
