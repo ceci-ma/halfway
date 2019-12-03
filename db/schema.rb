6fa8e7c65d54397f3670aadf10384c5619fca904
Molly Daguilar <mdaguilar66@gmail.com>
2019-12-03 17:43:59 +0000

    profile page css

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_12_03_112437) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,9 +69,9 @@ ActiveRecord::Schema.define(version: 2019_12_03_112437) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "price"
     t.float "latitude"
     t.float "longitude"
+    t.string "price"
     t.integer "commute_one"
     t.integer "commute_two"
     t.string "url"
