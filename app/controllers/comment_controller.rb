746e8de4eaf8b155114a0aa614893f146e1079e4
Lucas Chanet <lucaschanet@gmail.com>
2019-12-05 13:51:47 +0000

    chat js

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -5,7 +5,15 @@ class CommentController < ApplicationController
     @comment.user = current_user
     # @chatroom = Chatroom.find_by(user_a_id: current_user.id, user_b_id: friend_id) || Chatroom.find_by(user_a_id: friend_id, user_b_id: current_user.id)
     # @comment.chatroom = @chatroom
+
+    if @comment.user.avatar.attached?
+      @picture = @comment.user.avatar.key
+    else
+      @picture = "default_teifzq"
+    end
+
     @comment.save
+
 
     Pusher.trigger('comment-channel', 'new-comment', {
       id_a: @comment.chatroom.user_a_id.to_s,
@@ -14,7 +22,7 @@ class CommentController < ApplicationController
       comment: @comment.comment,
       first_name: current_user.first_name,
       created_at: @comment.created_at.strftime("%k:%M"),
-      picture: @comment.user.avatar.key
+      picture: @picture
     })
     redirect_to chatroom_path(@comment.chatroom)
     # render 'chatrooms/show'
